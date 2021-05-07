0e11a64f1ec79d8c906cb443444aaedd5acb774b
Github 864187149@qq.com <864187149@qq.com>
2021-05-07 16:56:01 +0800

    Game Alien

--- Python-Alien/alien_invasion.py
+++ Python-Alien/alien_invasion.py
@@ -18,18 +18,41 @@ class AlienInvasion:
         while True:
             self._check_events()
             self._updata_screen()
+            self.ship.update()
 
 
     def _check_events(self):
         '''键盘、鼠标响应事件'''
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    sys.exit()
+                    sys.exit()                           #退出程序
+
+                #飞船移动事件
+                elif event.type == pygame.KEYDOWN:           #键盘摁下的时候
+                    self._check_keydown_events(event)
+
+                elif event.type == pygame.KEYUP:              #键盘初始状态的时候
+                   self._check_keyup_events(event)
+
 
     def _updata_screen(self):
         self.screen.fill(self.settings.bg_color)       #每次循环时都会绘制屏幕
         self.ship.blitme()
         pygame.display.flip()          #让最近绘制的屏幕可见
+    
+    def _check_keyup_events(self,event):             #键盘初始状态的时候
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+    
+    def _check_keydown_events(self,event):            #键盘摁下的时候
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
 
 if __name__ == '__main__':
--- Python-Alien/settings.py
+++ Python-Alien/settings.py
@@ -3,4 +3,6 @@ class Setting:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
+
+        self.ship_speed = 1.5
       
